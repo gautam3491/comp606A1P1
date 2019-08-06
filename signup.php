27335197b39df8c7ccd42ee945fce871cbee584a
student <student@internal.rexnet.nz>
2019-08-07 09:25:41 +1200

    signup page design

--- signup.php
+++ signup.php
@@ -5,14 +5,14 @@
         background-color: grey;
         margin: 0 400 0 400;
     }
-    .log-in{
+    .sign-up{
         background-color: #EF496F;
     }
-    .log-info{
+    .sign-info{
         background-color: white;
         height: 680px;
     }
-    .log-btn{
+    .sign-btn{
         background-color: white;
     }
     .btn{
@@ -22,20 +22,32 @@
 </style>
 <body>
 <div class="container">
-    <div class="row log-in">
+    <div class="row sign-up">
         <div class="col text-white">
             <h3><a href="#" class="text-white"><</a></h3>
         </div>
         <div class="col text-center text-white">
-            <h3>Log in</h3>
+            <h3>Sign up</h3>
         </div>
         <div class="col text-right text-white">
             <h3>?</h3>
         </div>
     </div>
-    <div class="row log-info">
+    <div class="row sign-info">
         <div class="container">
-            <form action="login_page.php" method="POST">
+            <form action="signup_page.php" method="POST">
+                <div class="form-group">
+                    <label for="firstname">First Name:</label>
+                    <input type="text" class="form-control" id="firstname" name="firstname" required>
+                </div>
+                <div class="form-group">
+                    <label for="middlename">Middle Name:</label>
+                    <input type="text" class="form-control" id="middlename" name="middlename" required>
+                </div>
+                <div class="form-group">
+                    <label for="lastname">Last Name:</label>
+                    <input type="text" class="form-control" id="lastname" name="lastname" required>
+                </div>
                 <div class="form-group">
                     <label for="email">Email address:</label>
                     <input type="email" class="form-control" id="email" name="email" required>
@@ -44,13 +56,13 @@
                     <label for="pwd">Password:</label>
                     <input type="password" class="form-control" id="pwd" name="pwd" required>
                 </div>
-                <button type="submit" class="btn btn-default btn-block">Log in</button>
+                <button type="submit" class="btn btn-default btn-block">Sign up</button>
                 <br/>
-                <a href="signup.php"><input type="button" value="Not Registered Yet!!" class="btn btn-default btn-block"/></a>
+                <a href="login.php"><input type="button" value="Already Registered!!" class="btn btn-default btn-block"/></a>
             </form>
         </div>
     </div>
-    <div class="row log-btn">
+    <div class="row sign-btn">
         <!--<input type="submit" value="Log in" class="btn btn-light btn-block text-white"/>-->
     </div>
 </div>
