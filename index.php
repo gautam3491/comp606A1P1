1ee3fedbaef4ecd4557f4a21d328355b008cf618
student <student@internal.rexnet.nz>
2019-08-07 09:52:35 +1200

    added session

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 include_once("header.php");
 include_once("navbar.php");
+if (isset($_SESSION["username"]) && ($_SESSION["username"]!='')) {
+    $username = $_SESSION['username'];
+    
+}else
+{
+    $username = "";    
+}
+
 ?>
 <h1 class="text-white"> Making investment easy</h1>
+<h2></h2>
 <?php
 include_once("footer.php");
 ?>
--- login_page.php
+++ login_page.php
@@ -4,4 +4,14 @@
 $email = $_POST['email'];
 $password = $_POST['pwd'];
 
+$sql = "select * from customers where email='".$email."' and password='".$password."'";
+
+$result = $mysqli->query($sql);
+while($row = $result->fetch_row()){
+    session_start();
+    $_SESSION['username'] = $row[4];
+    header("Location: index.php");
+    //echo $_SESSION['username'];
+  }
+
 ?>
